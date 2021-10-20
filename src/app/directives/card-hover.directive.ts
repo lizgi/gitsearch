c1715366a8099381e800c6036da93e691250e00b
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-10-20 12:17:07 +0300

    adds directive

--- src/app/directives/card-hover.directive.ts
+++ src/app/directives/card-hover.directive.ts
@@ -8,7 +8,7 @@ export class CardHoverDirective {
   constructor(private el: ElementRef) { }
 
   @HostListener('mouseover') onOver() {
-    this.el.nativeElement.style.backgroundColor = 'rgb(135, 206, 250';
+    this.el.nativeElement.style.backgroundColor = 'rgb(135, 206, 245';
   }
 
   @HostListener('mouseout') onOut() {
